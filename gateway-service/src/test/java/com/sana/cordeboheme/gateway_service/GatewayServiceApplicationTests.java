d16dc723ec92f3cb74b9b5185a9fa21b2c1f1a0d
sana <sanadilnashin@gmail.com>
2026-07-24 22:44:56 +0530

    added gateway service and spotless plugin

--- gateway-service/src/test/java/com/sana/cordeboheme/gateway_service/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/sana/cordeboheme/gateway_service/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sana.cordeboheme.discovery_server;
+package com.sana.cordeboheme.gateway_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryServerApplicationTests {
+class GatewayServiceApplicationTests {
 
   @Test
   void contextLoads() {}
