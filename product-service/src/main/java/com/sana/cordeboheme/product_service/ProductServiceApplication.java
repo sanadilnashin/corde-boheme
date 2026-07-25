eedd92d86dcf290acceef3c2f7f01ca89dc5c5d2
sana <sanadilnashin@gmail.com>
2026-07-25 22:40:46 +0530

    setup added for db

--- product-service/src/main/java/com/sana/cordeboheme/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/sana/cordeboheme/product_service/ProductServiceApplication.java
@@ -2,9 +2,8 @@ package com.sana.cordeboheme.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ProductServiceApplication {
 
   public static void main(String[] args) {
